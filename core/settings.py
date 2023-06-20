24abf71f3a8e3bdbc0681734ae8348d811573809
AliMoradzadeh <moracode.master@gmail.com>
2023-06-20 03:39:30 +0330

    Setting up a channel layer with Redis

--- core/settings.py
+++ core/settings.py
@@ -170,3 +170,13 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
+
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('127.0.0.1', 6379)],
+        },
+    },
+}
